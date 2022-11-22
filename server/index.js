74562711df9ec92106c41784116c56e7c968ed93
cujo <cujothechief@gmail.com>
2022-11-22 08:51:29 +0000

    setting up MVC and working the server and database

--- server/index.js
+++ server/index.js
@@ -3,19 +3,19 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import * as dotenv from "dotenv";
+import postRoutes from "./routes/post.js";
 dotenv.config();
 
 const app = express();
-const port = process.evv.PORT || 5000;
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+const port = process.env.PORT || 5000;
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use("/posts", postRoutes);
 mongoose.connect(process.env.url, { useNewUrlParser: true, useUnifiedTopology: true })
    .then(() => {
       app.listen(port, () => {
-         console.log(`server running on port ${port}`)
-      })
+         console.log(`server is running on port ${port}`)
+      });
    })
-   .catch(err => console.log(err));
-
-mongoose.set("useFindAndModify", false);
+   .catch(err => console.log(err.message));
